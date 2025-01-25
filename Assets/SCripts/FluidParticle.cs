1e81ce3edcd44cef606c9050635e9df4846fc81f
Ilona <venusflytrapd.d@gmail.com>
2025-01-25 17:19:47 +0200

    Water top speed made

--- Assets/SCripts/FluidParticle.cs
+++ Assets/SCripts/FluidParticle.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class FluidParticle : MonoBehaviour
 {
-    public int ID;
+    [SerializeField] private float topSpeed;
+    private Rigidbody2D rb;
+    private bool _isInitialized = false;
+    public int ID {  get; set; }
+
+    public void Initialize()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        _isInitialized = true;
+    }
     public void SetFall(Vector3 fallPos) 
     { 
         transform.position = fallPos;
@@ -12,4 +21,12 @@ public class FluidParticle : MonoBehaviour
         if(other.collider.CompareTag("FluidDestroyer"))
             gameObject.SetActive(false);
 	}
+    private void Update()
+    {
+        if (!_isInitialized)
+            return;
+
+        if (rb.linearVelocity.magnitude > topSpeed)
+            rb.linearVelocity = rb.linearVelocity.normalized * topSpeed;
+    }
 }
