e2a4472a666d16f6f8fa44b9e3394a8b00f5974a
Ilona <venusflytrapd.d@gmail.com>
2025-01-26 00:44:01 +0300

    Started. Simple Scene lightning added

--- Assets/SCripts/Counter.cs
+++ Assets/SCripts/Counter.cs
@@ -4,29 +4,12 @@ public class Counter : MonoBehaviour
 {
     [SerializeField] private Cup _cup;
     [SerializeField] private Cursor _cursor;
-    [SerializeField] private Transform _orderTakePoint;
 
-    private void Awake()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("GlassBottom"))
         {
             _cursor.OnCupUnsnaped += HandleCupDelivered;
-            //Check if allowed
-            //if (!_cup.Follow)
-            //        return;
-
-            //If yes set to right position
-            //_cup.Delivered = true;
-            //_cup.Follow = false;
-            //_cup.SetPosition(_orderTakePoint);
-
-            //Count
-            //Debug.Log(_cup.CountFluid());
-            //Give results
         }
     }
     private void OnTriggerExit2D(Collider2D other)
