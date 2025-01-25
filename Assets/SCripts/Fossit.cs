915546a1e81a165784615b5d850a258908857c43
Ilona <venusflytrapd.d@gmail.com>
2025-01-25 16:34:07 +0200

    pool kinda fixed

--- Assets/SCripts/Fossit.cs
+++ Assets/SCripts/Fossit.cs
@@ -3,24 +3,24 @@ using UnityEngine;
 
 public class Fossit : MonoBehaviour
 {
-    [SerializeField] private FluidPooler objectPooler;
+    [SerializeField] private ObjectPooler objectPooler;
     [SerializeField] private Transform _particlesSpawnPosition;
     private IEnumerator _currentCourutine;
     private FluidParticle _currentParticle;
 
-    public void Open()
-    {
-        if (_currentCourutine != null)
-            return;
+ //   public void Open()
+ //   {
+ //       if (_currentCourutine != null)
+ //           return;
 
-        StartCoroutine(SpawnFluid());
-    }
-    private IEnumerator SpawnFluid()
-    {
-        while (true)
-        {
-			_currentParticle = objectPooler.GetPoolObject("Fluid");
-            _currentParticle.SetFall(_particlesSpawnPosition.position);
-		}
-	}
+ //       StartCoroutine(SpawnFluid());
+ //   }
+ //   private IEnumerator SpawnFluid()
+ //   {
+ //       while (true)
+ //       {
+	//		_currentParticle = objectPooler.GetPoolObject("Fluid");
+ //           _currentParticle.SetFall(_particlesSpawnPosition.position);
+	//	}
+	//}
 }
--- Assets/SCripts/ObjectPooler.cs
+++ Assets/SCripts/ObjectPooler.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
-using System;
 using System.Collections.Generic;
 
 [System.Serializable]
 public class PoolSettings
 {
     public string tag;
-    public FluidParticle prefab;
+    [field: SerializeField] public FluidParticle prefab;
     public int poolSize;
 }
 
-public class FluidPooler : MonoBehaviour
+public class ObjectPooler : MonoBehaviour
 {
-    public static FluidPooler Instance;
+    public static ObjectPooler Instance;
+    public FluidParticle prefabForFluidParticle;
 
     public List<PoolSettings> pools;
     private Dictionary<string, Queue<FluidParticle>> poolDictionary = new Dictionary<string, Queue<FluidParticle>>();
@@ -22,21 +22,31 @@ public class FluidPooler : MonoBehaviour
 
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        Debug.Log(this.enabled);
         foreach (PoolSettings settings in pools)
         {
             Queue<FluidParticle> pool = new Queue<FluidParticle>();
 
             for (int i = 0; i < settings.poolSize; i++)
             {
-				FluidParticle obj = Instantiate(settings.prefab);
+                if (settings.prefab == null) settings.prefab = prefabForFluidParticle;
+                FluidParticle obj = Instantiate(settings.prefab, transform);
                 obj.gameObject.SetActive(false);
                 pool.Enqueue(obj);
             }
 
             poolDictionary.Add(settings.tag, pool);
         }
-
-        Instance = this;
+        Debug.Log(this.enabled);
     }
 
     public FluidParticle GetPoolObject(string tag)
@@ -53,4 +63,5 @@ public class FluidPooler : MonoBehaviour
         currentQueue.Enqueue(pooledObject);
         return pooledObject;
     }
+
 }
